9dd939dbb74155b5962c0e2b036d37931784c11c
Jefferson <jefferson.medina@dhigroupinc.com>
2023-05-04 21:56:44 -0500

    craete loading

--- src/app/table/table-by-pages/table-by-pages.component.ts
+++ src/app/table/table-by-pages/table-by-pages.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, mergeMap, Observable, of } from 'rxjs';
 import { ItemsRender, ParamsResult } from '../model/items';
 import { itemsGetAll } from '../store/actions/items.action';
 import { selectAllItems, State } from '../store/reducers/items.reducer';
+import { CLEAROBJ, LOADING } from '../utils/functions';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-table-by-pages',
   templateUrl: './table-by-pages.component.html',
   styleUrls: ['./table-by-pages.component.scss']
 })
-export class TableByPagesComponent {
+export class TableByPagesComponent implements OnInit {
 
-  data$: Observable<ItemsRender> = this.store.select(selectAllItems);
+  data$: Observable<ItemsRender> = this.store.select(selectAllItems).pipe(CLEAROBJ);
   offsetold: number = 0;
   limit: number = 5;
+  loading$: Observable<boolean> = of(true)
 
   constructor(private store: Store<State>) { }
+
+  ngOnInit(): void {
+    this.loading$ = this.store.select(selectAllItems).pipe(debounceTime(200), LOADING);
+
+    /* if you want simulate delay time */
+    /*setTimeout(()=>{
+      this.loading$ = this.store.select(selectAllItems).pipe(debounceTime(200), LOADING);
+    },5000)*/
+  }
 
   nextPage(next_offset: number): void {
     const pagesKitNxt = this.buildPagination(next_offset)
--- src/app/table/utils/functions.ts
+++ src/app/table/utils/functions.ts
@@ -4,4 +4,11 @@ import { of } from "rxjs";
 
  const CLEANOBJECT =  filter((val:Items) => !!val)
  const MAPOBJECT =  map((val) => val as Items)
+ const SETLOADING = map((list:Items) => !list.success as boolean)
+
  export const CLEAROBJ = mergeMap((stream) => of(stream).pipe(MAPOBJECT,CLEANOBJECT))
+ export const LOADING = mergeMap((stream) => of(stream).pipe(MAPOBJECT,CLEANOBJECT,SETLOADING))
+
+
+
+ 
