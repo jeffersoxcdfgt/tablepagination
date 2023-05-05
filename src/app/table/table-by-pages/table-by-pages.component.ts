c8ccbdf6863ef7a2c5c0165ef1d682d571cd1f64
Jefferson <jefferson.medina@dhigroupinc.com>
2023-05-04 20:11:38 -0500

    Adding no rows display

--- src/app/table/table-by-pages/table-by-pages.component.ts
+++ src/app/table/table-by-pages/table-by-pages.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { ItemsRender } from '../model/items';
+import { ItemsRender, ParamsResult } from '../model/items';
 import { itemsGetAll } from '../store/actions/items.action';
 import { selectAllItems, State } from '../store/reducers/items.reducer';
 
@@ -12,34 +12,31 @@ import { selectAllItems, State } from '../store/reducers/items.reducer';
 })
 export class TableByPagesComponent {
 
-  data$:Observable<ItemsRender> =this.store.select(selectAllItems);
-  offsetold:number = 0;
+  data$: Observable<ItemsRender> = this.store.select(selectAllItems);
+  offsetold: number = 0;
   limit: number = 5;
 
-  constructor(private store :Store<State>){  }
+  constructor(private store: Store<State>) { }
 
-  nextPage(next_offset:number, total_records:number):void{
-      const pagesKit = {
-        metadata:{ 
-          _limit : this.limit,
-          offset : next_offset
-        }
-      }
-      this.offsetold = next_offset - this.limit;
-      this.store.dispatch(itemsGetAll(pagesKit)); 
+  nextPage(next_offset: number): void {
+    const pagesKitNxt = this.buildPagination(next_offset)
+    this.offsetold = next_offset - this.limit;
+    this.store.dispatch(itemsGetAll(pagesKitNxt));
   }
 
-  previousPage(offsetold:number):void{
+  previousPage(offsetold: number): void {
+    const pagesKitPrv = this.buildPagination(offsetold)
+    this.store.dispatch(itemsGetAll(pagesKitPrv));
+    this.offsetold = offsetold - this.limit;
+  }
 
-    const pagesKit = {
-      metadata:{ 
-        _limit : this.limit,
-        offset : offsetold
+  buildPagination(page: number): ParamsResult {
+    return {
+      metadata: {
+        _limit: this.limit,
+        offset: page
       }
     }
-    this.store.dispatch(itemsGetAll(pagesKit)); 
-    this.offsetold =  offsetold - this.limit
-
   }
 
 }
