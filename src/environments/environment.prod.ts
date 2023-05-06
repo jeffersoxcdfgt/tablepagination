126b3e928e69e282b392b267093a04de7ddc3ac7
Jefferson <jefferson.medina@dhigroupinc.com>
2023-05-06 12:23:59 -0500

    Updating config files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    urlItems:'https://secure.ultracart.com/rest/v2',
+    urlItems:'http://localhost:1338',
     xultracartsimplekey:'e6d05e7649934c0187e32fe65e2e5300efcb4786d67cf30187e32fe65e2e5300',
     xultracartapiversion:'2017-03-01'
   };
