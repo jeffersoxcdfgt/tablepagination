5b493ce3a8c3b1f49db74595b38461c490d2a067
Jefferson <jefferson.medina@dhigroupinc.com>
2023-05-10 10:08:46 -0500

    update table process

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,16 +1,10 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent implements OnChanges {
-
+export class LoadingComponent {
   @Input() loading : boolean | null = true;
-
-  ngOnChanges(changes: SimpleChanges) {
-    console.log(changes)
-  }
-
 }
